e65840d0b34fca9914a8cd9674724dc305d88a93
Mark John Saspa <markjonhsaspa@gmail.com>
2024-09-21 17:52:02 +0800

    fix config

--- pacific-nursing-solution/vite.config.js
+++ pacific-nursing-solution/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      axios: require.resolve('axios'),
+    },
+  },
   plugins: [react(), svgr()],
   server: {
     port: 5174,
