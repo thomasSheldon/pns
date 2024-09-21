bdbb76b8a6db8372cd54b05222399b83fde71762
Mark John Saspa <markjonhsaspa@gmail.com>
2024-09-21 22:35:29 +0800

    remove unnesery react

--- pacific-nursing-solution/src/App.jsx
+++ pacific-nursing-solution/src/App.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import MainPage from './components/pages/MainPage';
-import axios from 'axios';
-import PropTypes from 'prop-types';
 import './App.css';
 
 function App() {
